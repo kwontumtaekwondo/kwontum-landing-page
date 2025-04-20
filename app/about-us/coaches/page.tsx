1bbae95b731ac9781af0a6001b4ce0721751430e
Lim Yu De <lim.yude1@gmail.com>
2025-04-20 21:48:18 +0800

    Fixed Coaches page alignment issues

--- app/about-us/coaches/page.tsx
+++ app/about-us/coaches/page.tsx
@@ -7,8 +7,8 @@ import CoachCard from "@/components/coaches/CoachCard";
 const CoachesPage = () => {
     return (
         <div className="py-12 px-6 bg-gray-50 min-h-screen">
-            <h1 className="text-6xl xs:7xl lg:text-4xl font-bold font-dolceVita mb-4 text-kwontum-darkRed ml-44 text-center">Our Coaches</h1>
-            <div className="flex flex-wrap justify-center gap-8 ml-44">
+            <h1 className="text-6xl xs:7xl lg:text-4xl font-bold font-dolceVita mb-4 text-kwontum-darkRed text-center">Our Coaches</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 max-w-8xl mx-auto">
                 {/* Render a CoachCard for each coach */}
                 {coachesData.map((coach, index) => (
                     <CoachCard
